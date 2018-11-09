6500571866d0aa95d65c9564571e831a9b12127d
Maxime Britto <maxime@purplegiraffe.fr>
2018-11-09 16:44:04 +0100

    Mini cours : 127_Votre_premier_ordre_en_Kotlin

--- mini-cours/blocnote/app/src/main/java/fr/purplegiraffe/blocnote/MainActivity.kt
+++ mini-cours/blocnote/app/src/main/java/fr/purplegiraffe/blocnote/MainActivity.kt
@@ -2,6 +2,7 @@ package fr.purplegiraffe.blocnote
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 
 class MainActivity : AppCompatActivity() {
@@ -12,7 +13,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun addButtonTouched(button:View) {
-        
+        Log.i("Maxime", "Coucou à tous !")
     }
 }
 
