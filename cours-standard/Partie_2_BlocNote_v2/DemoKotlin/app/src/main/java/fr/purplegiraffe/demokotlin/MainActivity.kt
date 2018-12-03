cabb61da71517f46823abb4e90366085ec77c34a
Maxime Britto <maxime@purplegiraffe.fr>
2018-12-03 16:21:41 +0100

    233_Valeurs_et_variables

--- cours-standard/Partie_2_BlocNote_v2/DemoKotlin/app/src/main/java/fr/purplegiraffe/demokotlin/MainActivity.kt
+++ cours-standard/Partie_2_BlocNote_v2/DemoKotlin/app/src/main/java/fr/purplegiraffe/demokotlin/MainActivity.kt
@@ -3,6 +3,7 @@ package fr.purplegiraffe.demokotlin
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,6 +13,9 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun goButtonTouched(button:View) {
-        
+        val firstname = firstnameField.text
+        val lastname = lastnameField.text
+        val fullname = "${firstname} ${lastname}"
+        resultView.text = "Bonjour ${fullname}"
     }
 }
