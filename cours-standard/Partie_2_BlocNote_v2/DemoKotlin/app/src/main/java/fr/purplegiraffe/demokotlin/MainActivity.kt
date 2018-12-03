b2b534fb5a6767dc51c50ffe3fb410bd34449740
Maxime Britto <maxime@purplegiraffe.fr>
2018-12-03 16:03:28 +0100

    232_Preparation_du_projet_de_demo

---
+++ cours-standard/Partie_2_BlocNote_v2/DemoKotlin/app/src/main/java/fr/purplegiraffe/demokotlin/MainActivity.kt
@@ -0,0 +1,17 @@
+package fr.purplegiraffe.demokotlin
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun goButtonTouched(button:View) {
+        
+    }
+}
