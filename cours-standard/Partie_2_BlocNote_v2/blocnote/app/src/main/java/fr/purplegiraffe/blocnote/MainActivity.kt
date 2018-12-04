45e6ecdfaf16e7dde475c4e2ca08ec96986186bf
Maxime Britto <maxime@purplegiraffe.fr>
2018-12-04 12:09:06 +0100

    251_Mise_en_page_adaptative

---
+++ cours-standard/Partie_2_BlocNote_v2/blocnote/app/src/main/java/fr/purplegiraffe/blocnote/MainActivity.kt
@@ -0,0 +1,23 @@
+package fr.purplegiraffe.blocnote
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun addButtonTouched(button:View) {
+        val userInput = userField.text
+        val existingText = fullTextView.text
+        fullTextView.text = "$existingText\n$userInput"
+        userField.text = null
+    }
+}
+
