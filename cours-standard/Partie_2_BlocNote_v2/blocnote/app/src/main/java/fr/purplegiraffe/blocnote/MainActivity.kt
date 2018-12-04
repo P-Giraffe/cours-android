e3dfa3ec4072bf6320b9baeab8ef4cca87c34111
Maxime Britto <maxime@purplegiraffe.fr>
2018-12-04 12:58:26 +0100

    254_Ameliorations_et_qualite_de_code

--- cours-standard/Partie_2_BlocNote_v2/blocnote/app/src/main/java/fr/purplegiraffe/blocnote/MainActivity.kt
+++ cours-standard/Partie_2_BlocNote_v2/blocnote/app/src/main/java/fr/purplegiraffe/blocnote/MainActivity.kt
@@ -3,28 +3,34 @@ package fr.purplegiraffe.blocnote
 import android.content.Context
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     var fulltext = ""
+    val SETTINGS_FILE_NAME = "fr.purplegiraffe.blocnote"
+    val FULLTEXT_SETTINGS_KEY = "fulltext"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val sharedPreferences = getSharedPreferences("fr.purplegiraffe.blocnote", Context.MODE_PRIVATE)
-        fulltext = sharedPreferences.getString("fulltext", "")
+        val sharedPreferences = getSharedPreferences(SETTINGS_FILE_NAME, Context.MODE_PRIVATE)
+        fulltext = sharedPreferences.getString(FULLTEXT_SETTINGS_KEY, "")
         fullTextView.text = fulltext
     }
 
     fun addButtonTouched(button:View) {
         val userInput = userField.text
         fulltext = "$fulltext\n$userInput"
-        val sharedPreferencesEditor = getSharedPreferences("fr.purplegiraffe.blocnote", Context.MODE_PRIVATE).edit()
-        sharedPreferencesEditor.putString("fulltext", fulltext)
-        sharedPreferencesEditor.apply()
+        saveTextToDB()
         fullTextView.text = fulltext
         userField.text = null
+    }
+
+    fun saveTextToDB() {
+        val sharedPreferencesEditor = getSharedPreferences(SETTINGS_FILE_NAME, Context.MODE_PRIVATE).edit()
+        sharedPreferencesEditor.putString(FULLTEXT_SETTINGS_KEY, fulltext)
+        sharedPreferencesEditor.apply()
     }
 }
 
