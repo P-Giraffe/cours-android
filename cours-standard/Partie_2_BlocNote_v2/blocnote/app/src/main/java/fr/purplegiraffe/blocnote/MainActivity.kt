a31b71aec35063de4a1c82cbe161bc878cbcfa25
Maxime Britto <maxime@purplegiraffe.fr>
2018-12-04 12:49:41 +0100

    253_Sauvegarde_du_bloc_note

--- cours-standard/Partie_2_BlocNote_v2/blocnote/app/src/main/java/fr/purplegiraffe/blocnote/MainActivity.kt
+++ cours-standard/Partie_2_BlocNote_v2/blocnote/app/src/main/java/fr/purplegiraffe/blocnote/MainActivity.kt
@@ -1,5 +1,6 @@
 package fr.purplegiraffe.blocnote
 
+import android.content.Context
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -11,11 +12,17 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val sharedPreferences = getSharedPreferences("fr.purplegiraffe.blocnote", Context.MODE_PRIVATE)
+        fulltext = sharedPreferences.getString("fulltext", "")
+        fullTextView.text = fulltext
     }
 
     fun addButtonTouched(button:View) {
         val userInput = userField.text
         fulltext = "$fulltext\n$userInput"
+        val sharedPreferencesEditor = getSharedPreferences("fr.purplegiraffe.blocnote", Context.MODE_PRIVATE).edit()
+        sharedPreferencesEditor.putString("fulltext", fulltext)
+        sharedPreferencesEditor.apply()
         fullTextView.text = fulltext
         userField.text = null
     }
