22077576f4ea3bc81efc31a553cfe5a01366a0b9
Maxime Britto <maxime@purplegiraffe.fr>
2018-12-07 11:24:07 +0100

    324_Variables_et_types_explicites

--- cours-standard/ApprentissageKotlin/src/variables.kt
+++ cours-standard/ApprentissageKotlin/src/variables.kt
@@ -1,3 +1,10 @@
 fun main(args: Array<String>) {
+    
+}
 
+fun valTypes() {
+    val explicitString : String = "Sheldon"
+    val implicitString = "Leonard"
+    val explicitInt : Int = 20
+    val implicitInt = 20
 }
