587556fabff56d991442d56fd56e988cc212d7f2
Maxime Britto <maxime@purplegiraffe.fr>
2018-12-07 11:13:16 +0100

    323_when_suite

--- cours-standard/ApprentissageKotlin/src/controlFlow.kt
+++ cours-standard/ApprentissageKotlin/src/controlFlow.kt
@@ -1,11 +1,5 @@
 fun main(args: Array<String>) {
-    val age = 17
-    when (age) {
-        in 0..17 -> print("Vous êtes mineur(e)")
-        18 -> print("Vous venez de devenir majeur(e) en france")
-        19,20 -> print("Vous allez bientôt devenir majeur(e) aux USA")
-        else -> print("Vous êtes majeur(e)")
-    }
+
 }
 
 fun ifelse() {
@@ -18,5 +12,15 @@ fun ifelse() {
         print("Vous allez bientôt devenir majeur(e) aux USA")
     } else {
         print("Vous êtes majeur(e)")
+    }
+}
+
+fun whenElse() {
+    val age = 17
+    when (age) {
+        in 0..17 -> print("Vous êtes mineur(e)")
+        18 -> print("Vous venez de devenir majeur(e) en france")
+        19,20 -> print("Vous allez bientôt devenir majeur(e) aux USA")
+        else -> print("Vous êtes majeur(e)")
     }
 }
