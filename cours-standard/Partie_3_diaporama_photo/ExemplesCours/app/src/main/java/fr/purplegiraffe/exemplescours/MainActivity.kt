02108f5f97cae324f8d05d52d3282136f1c68252
Maxime Britto <maxime@purplegiraffe.fr>
2018-12-09 18:09:38 +0100

    334_Restaurer_une_activite_depuis_le_cache

--- cours-standard/Partie_3_diaporama_photo/ExemplesCours/app/src/main/java/fr/purplegiraffe/exemplescours/MainActivity.kt
+++ cours-standard/Partie_3_diaporama_photo/ExemplesCours/app/src/main/java/fr/purplegiraffe/exemplescours/MainActivity.kt
@@ -2,21 +2,42 @@ package fr.purplegiraffe.exemplescours
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
 import android.util.Log
 import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-    var clickCount = 0
+    var clickCount:Int = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        restoreState(savedInstanceState)
         Log.i("ActivityLifecycle", "onCreate")
     }
 
-    public fun buttonClick(button:View) {
+    fun buttonClick(button:View) {
         clickCount = clickCount + 1
+        updateDisplay()
+    }
+
+    fun updateDisplay() {
         textView.text = "${clickCount} clic(s)"
+    }
+
+    override fun onSaveInstanceState(outState: Bundle?) {
+        super.onSaveInstanceState(outState)
+        outState?.putInt("clickCount", clickCount)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle?) {
+        super.onRestoreInstanceState(savedInstanceState)
+        restoreState(savedInstanceState)
+    }
+
+    private fun restoreState(savedInstanceState: Bundle?) {
+        clickCount = savedInstanceState?.getInt("clickCount") ?: 0
+        updateDisplay()
     }
 
     override fun onResume() {
