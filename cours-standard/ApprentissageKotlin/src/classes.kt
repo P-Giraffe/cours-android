880dbc3ba5324b2c6b9f045f2b8b3ae580841e13
Maxime Britto <maxime@purplegiraffe.fr>
2018-12-10 15:40:51 +0100

    3211_Classes_et_fonctions

--- cours-standard/ApprentissageKotlin/src/classes.kt
+++ cours-standard/ApprentissageKotlin/src/classes.kt
@@ -9,11 +9,16 @@ fun main(args: Array<String>) {
 
     p1.score = 10
     p2.score = 20
-    println("${p1.nickname}, score: ${p1.score}")
-    println("${p2.nickname}, score: ${p2.score}")
+
+    p1.printYourself()
+    p2.printYourself()
 }
 
 class Player (val nickname:String) {
     var score:Int = 0
     var health:Int = 100
+
+    fun printYourself() {
+        println("${this.nickname} | Score : ${this.score} | Santé : ${this.health}")
+    }
 }
