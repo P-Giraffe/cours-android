cd6aad33d5f4ee3ef75908b60804a56cd97a09c5
Maxime Britto <maxime@purplegiraffe.fr>
2018-12-27 17:07:05 +0100

    364_Gestion_du_bouton_suivant

--- cours-standard/Partie_3_diaporama_photo/Slideshow/app/src/main/java/fr/purplegiraffe/slideshow/SlideshowActivity.kt
+++ cours-standard/Partie_3_diaporama_photo/Slideshow/app/src/main/java/fr/purplegiraffe/slideshow/SlideshowActivity.kt
@@ -2,6 +2,8 @@ package fr.purplegiraffe.slideshow
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_slideshow.*
 
 class SlideshowActivity : AppCompatActivity() {
     val photoList = arrayOf(R.drawable.sushi, R.drawable.chat1, R.drawable.chien1, R.drawable.chat2, R.drawable.chien2, R.drawable.chat3)
@@ -10,5 +12,17 @@ class SlideshowActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_slideshow)
+    }
+
+    fun previousButtonTouched(button:View) {
+
+    }
+
+    fun nextButtonTouched(button: View) {
+        currentPhotoIndex = currentPhotoIndex + 1
+        if (currentPhotoIndex >= photoList.size) {
+            currentPhotoIndex = 0
+        }
+        imageView.setImageResource(photoList[currentPhotoIndex])
     }
 }
