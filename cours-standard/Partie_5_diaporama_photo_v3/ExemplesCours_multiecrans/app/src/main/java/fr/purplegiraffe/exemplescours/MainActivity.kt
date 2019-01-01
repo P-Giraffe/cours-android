d0090e664631a2c90b7b5bcec689351e4880e28f
Maxime Britto <maxime@purplegiraffe.fr>
2019-01-01 17:22:44 +0100

    Preparation du tp pour la partie 5

---
+++ cours-standard/Partie_5_diaporama_photo_v3/ExemplesCours_multiecrans/app/src/main/java/fr/purplegiraffe/exemplescours/MainActivity.kt
@@ -0,0 +1,93 @@
+package fr.purplegiraffe.exemplescours
+
+import android.content.Intent
+import android.net.Uri
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.os.PersistableBundle
+import android.util.Log
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    var clickCount:Int = 0
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        restoreState(savedInstanceState)
+        Log.i("ActivityLifecycle", "onCreate")
+    }
+
+    fun buttonClick(button:View) {
+        clickCount = clickCount + 1
+        updateDisplay()
+    }
+
+    fun settingsButtonTouched(button:View) {
+        val intent = Intent(this, SettingsActivity::class.java)
+        intent.putExtra("clickCount", clickCount)
+        startActivity(intent)
+    }
+
+    fun purpleGiraffeButtonTouched(button:View) {
+        val webpage: Uri = Uri.parse("https://www.purplegiraffe.fr")
+        val intent = Intent(Intent.ACTION_VIEW, webpage)
+        if (intent.resolveActivity(packageManager) != null) {
+            startActivity(intent)
+        }
+    }
+
+    fun updateDisplay() {
+        if (clickCount == 0) {
+            textView.text = getString(R.string.no_click)
+        } else {
+            textView.text = getString(R.string.click_count, clickCount)
+            textView.text = resources.getQuantityString(R.plurals.click_count_p, clickCount, clickCount)
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle?) {
+        super.onSaveInstanceState(outState)
+        outState?.putInt("clickCount", clickCount)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle?) {
+        super.onRestoreInstanceState(savedInstanceState)
+        restoreState(savedInstanceState)
+    }
+
+    private fun restoreState(savedInstanceState: Bundle?) {
+        clickCount = savedInstanceState?.getInt("clickCount") ?: 0
+        updateDisplay()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.i("ActivityLifecycle", "onResume")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.i("ActivityLifecycle", "onStart")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.i("ActivityLifecycle", "onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.i("ActivityLifecycle", "onStop")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.i("ActivityLifecycle", "onRestart")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.i("ActivityLifecycle", "onDestroy")
+    }
+}
---
+++ cours-standard/Partie_5_diaporama_photo_v3/Slideshow/app/src/main/java/fr/purplegiraffe/slideshow/SlideshowActivity.kt
@@ -0,0 +1,75 @@
+package fr.purplegiraffe.slideshow
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.os.CountDownTimer
+import android.view.View
+import kotlinx.android.synthetic.main.activity_slideshow.*
+
+class SlideshowActivity : AppCompatActivity() {
+    val photoList = arrayOf(R.drawable.sushi, R.drawable.chat1, R.drawable.chien1, R.drawable.chat2, R.drawable.chien2, R.drawable.chat3)
+    var currentPhotoIndex = 0
+    var countDownTimer:CountDownTimer? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_slideshow)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        startSlideshow()
+    }
+
+    fun previousButtonTouched(button:View) {
+        stopSlideshow()
+        currentPhotoIndex = currentPhotoIndex - 1
+        if (currentPhotoIndex < 0) {
+            currentPhotoIndex = photoList.size - 1
+        }
+        imageView.setImageResource(photoList[currentPhotoIndex])
+        startSlideshow()
+    }
+
+    fun nextButtonTouched(button: View) {
+        stopSlideshow()
+        showNextPhoto()
+        startSlideshow()
+    }
+
+    fun stopSlideshow() {
+        countDownTimer?.cancel()
+        countDownTimer = null
+    }
+
+    fun startSlideshow() {
+        countDownTimer = object : CountDownTimer(3000,3000) {
+            override fun onFinish() {
+                showNextPhoto()
+                startSlideshow()
+            }
+
+            override fun onTick(millisUntilFinished: Long) { }
+        }
+        countDownTimer?.start()
+    }
+
+    fun showNextPhoto() {
+        currentPhotoIndex = currentPhotoIndex + 1
+        if (currentPhotoIndex >= photoList.size) {
+            currentPhotoIndex = 0
+        }
+        imageView.setImageResource(photoList[currentPhotoIndex])
+    }
+
+    override fun onSaveInstanceState(outState: Bundle?) {
+        super.onSaveInstanceState(outState)
+        outState?.putInt("currentPhotoIndex", currentPhotoIndex)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle?) {
+        super.onRestoreInstanceState(savedInstanceState)
+        currentPhotoIndex = savedInstanceState?.getInt("currentPhotoIndex") ?: 0
+        imageView.setImageResource(photoList[currentPhotoIndex])
+    }
+}
