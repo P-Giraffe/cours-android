a63fb8b6854a7169c2572adf522866391c23c19e
Maxime Britto <maxime@purplegiraffe.fr>
2019-01-01 17:10:33 +0100

    532_Choix_du_menu_pour_une_activite

--- cours-standard/Partie_4_diaporama_photo_v2/Exemple_AppBar/app/src/main/java/fr/purplegiraffe/exemple_appbar/MainActivity.kt
+++ cours-standard/Partie_4_diaporama_photo_v2/Exemple_AppBar/app/src/main/java/fr/purplegiraffe/exemple_appbar/MainActivity.kt
@@ -2,11 +2,17 @@ package fr.purplegiraffe.exemple_appbar
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return super.onCreateOptionsMenu(menu)
     }
 }
