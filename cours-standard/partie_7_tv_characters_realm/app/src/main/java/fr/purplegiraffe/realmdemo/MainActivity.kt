c78ddfdbbcd72216e97bd0fe94fadb1539809eb2
Maxime Britto <maxime@purplegiraffe.fr>
2019-05-25 15:35:50 +0200

    734_Acces_a_la_base_de_donnees

--- cours-standard/partie_7_tv_characters_realm/app/src/main/java/fr/purplegiraffe/realmdemo/MainActivity.kt
+++ cours-standard/partie_7_tv_characters_realm/app/src/main/java/fr/purplegiraffe/realmdemo/MainActivity.kt
@@ -4,15 +4,22 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import io.realm.Realm
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    lateinit var _realm:Realm
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        _realm = Realm.getDefaultInstance()
     }
 
     fun onAddButtonTouched(button:View) {
+
+    }
+
+    fun loadDogsFromDataBase() {
 
     }
 }
