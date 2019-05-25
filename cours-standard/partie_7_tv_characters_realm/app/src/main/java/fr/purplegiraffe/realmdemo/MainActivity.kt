8e1d949b038075cee9c792c4288da6d70af867f2
Maxime Britto <maxime@purplegiraffe.fr>
2019-05-25 15:57:12 +0200

    736_Retrouver_un_objet_dans_la_base

--- cours-standard/partie_7_tv_characters_realm/app/src/main/java/fr/purplegiraffe/realmdemo/MainActivity.kt
+++ cours-standard/partie_7_tv_characters_realm/app/src/main/java/fr/purplegiraffe/realmdemo/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.view.View
 import io.realm.Realm
 import kotlinx.android.synthetic.main.activity_main.*
+import java.lang.StringBuilder
 
 class MainActivity : AppCompatActivity() {
     lateinit var _realm:Realm
@@ -13,6 +14,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         _realm = Realm.getDefaultInstance()
+        loadDogsFromDataBase()
     }
 
     fun onAddButtonTouched(button:View) {
@@ -26,10 +28,19 @@ class MainActivity : AppCompatActivity() {
             _realm.commitTransaction()
 
             ui_nameInput.text.clear()
+
+            loadDogsFromDataBase()
         }
     }
 
     fun loadDogsFromDataBase() {
+        val dogList = _realm.where(Dog::class.java).findAll()
 
+        val listBuilder = StringBuilder("Liste de chiens :\n")
+        for (dog in dogList) {
+            listBuilder.append("- ${dog.name}\n")
+        }
+
+        ui_dataView.text = listBuilder.toString()
     }
 }
