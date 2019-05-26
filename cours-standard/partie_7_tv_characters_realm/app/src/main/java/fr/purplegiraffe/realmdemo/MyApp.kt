573d21e9266b54b2ed9db08f575fd559f8c25f65
Maxime Britto <maxime@purplegiraffe.fr>
2019-05-26 23:55:30 +0200

    739_Personnaliser_les_reglages_de_Realm

--- cours-standard/partie_7_tv_characters_realm/app/src/main/java/fr/purplegiraffe/realmdemo/MyApp.kt
+++ cours-standard/partie_7_tv_characters_realm/app/src/main/java/fr/purplegiraffe/realmdemo/MyApp.kt
@@ -2,10 +2,13 @@ package fr.purplegiraffe.realmdemo
 
 import android.app.Application
 import io.realm.Realm
+import io.realm.RealmConfiguration
 
 class MyApp : Application() {
     override fun onCreate() {
         super.onCreate()
         Realm.init(this)
+        val config = RealmConfiguration.Builder().deleteRealmIfMigrationNeeded().inMemory().build()
+        Realm.setDefaultConfiguration(config)
     }
 }
