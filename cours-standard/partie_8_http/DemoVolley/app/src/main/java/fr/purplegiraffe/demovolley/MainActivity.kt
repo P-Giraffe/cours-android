b98bda1e14b9a414a3fe702af6d72790cfc9096e
Maxime Britto <maxime@purplegiraffe.fr>
2019-06-25 16:40:34 +0200

    833_Creation_de_la_RequestQueue

--- cours-standard/partie_8_http/DemoVolley/app/src/main/java/fr/purplegiraffe/demovolley/MainActivity.kt
+++ cours-standard/partie_8_http/DemoVolley/app/src/main/java/fr/purplegiraffe/demovolley/MainActivity.kt
@@ -2,11 +2,22 @@ package fr.purplegiraffe.demovolley
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.android.volley.toolbox.Volley
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-
+    val requestQueue = Volley.newRequestQueue(this)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        downloadButton.setOnClickListener {
+            val url = urlField.text
+            launchDownloadRequest(url.toString())
+        }
+    }
+
+    fun launchDownloadRequest(url:String){
+
     }
 }
