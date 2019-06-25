b38f9f39ae58ca41c066da79873f378422873bd6
Maxime Britto <maxime@purplegiraffe.fr>
2019-06-25 16:58:34 +0200

    834_Envoyer_une_requete_simple

--- cours-standard/partie_8_http/DemoVolley/app/src/main/java/fr/purplegiraffe/demovolley/MainActivity.kt
+++ cours-standard/partie_8_http/DemoVolley/app/src/main/java/fr/purplegiraffe/demovolley/MainActivity.kt
@@ -2,14 +2,19 @@ package fr.purplegiraffe.demovolley
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.android.volley.Request
+import com.android.volley.RequestQueue
+import com.android.volley.Response
+import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-    val requestQueue = Volley.newRequestQueue(this)
+    lateinit var requestQueue:RequestQueue
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        requestQueue = Volley.newRequestQueue(this)
 
         downloadButton.setOnClickListener {
             val url = urlField.text
@@ -18,6 +23,13 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun launchDownloadRequest(url:String){
+        val request = StringRequest(Request.Method.GET, url, Response.Listener { body:String ->
+            outputView.text = body
+        }, Response.ErrorListener {
+            outputView.text = "Error"
+        })
 
+        outputView.text = "Chargement..."
+        requestQueue.add(request)
     }
 }
