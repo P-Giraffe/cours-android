d6b33725384ad3e7d4176eabd2763730fb9e08f0
Maxime Britto <maxime@purplegiraffe.fr>
2019-06-25 15:28:45 +0200

    821_by_lazy

--- cours-standard/partie_8_http/app/src/main/java/fr/purplegiraffe/kotlin/MainActivity.kt
+++ cours-standard/partie_8_http/app/src/main/java/fr/purplegiraffe/kotlin/MainActivity.kt
@@ -2,10 +2,20 @@ package fr.purplegiraffe.kotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    val calculatedValue:Int by lazy {
+        Log.i("LAZY", "Calcul de la valeur en cours")
+
+        var bigValue = 1
+        for (i in 1..10) {
+            bigValue *= i
+        }
+        bigValue
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,6 +26,9 @@ class MainActivity : AppCompatActivity() {
             statusLabel.text = "Vous avez touché le bouton 2"
             buttonTouched.visibility = View.GONE
         }
+
+        Log.i("LAZY", "$calculatedValue")
+        Log.i("LAZY", "$calculatedValue")
     }
 
 //    public fun button2Touched(button: View) {
